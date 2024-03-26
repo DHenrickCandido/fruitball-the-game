f25620aa4870f4d282780dd1099d217e76d711ba
Diego Henrick <diegohenrick@Candidos-Macbook-Pro.local>
2024-03-26 08:49:53 -0300

    first commit

---
+++ fruitball/fruitball/fruitballApp.swift
@@ -0,0 +1,17 @@
+//
+//  fruitballApp.swift
+//  fruitball
+//
+//  Created by Diego Henrick on 21/03/24.
+//
+
+import SwiftUI
+
+@main
+struct fruitballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            GameSceneView()
+        }
+    }
+}
