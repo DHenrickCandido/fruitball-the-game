e45b5dba7f0758186031085e33c7386f6972af02
MuriloPasq <murilopasquim@gmail.com>
2024-04-22 10:52:38 -0300

    Implementação do Crashanalytics, Firebase e AdMob

---
+++ jugglingfruits/AdMob/IntersticialAdView.swift
@@ -0,0 +1,8 @@
+//
+//  IntersticialAdView.swift
+//  jugglingfruits
+//
+//  Created by Murilo Correa Pasquim on 22/04/24.
+//
+
+import Foundation
---
+++ jugglingfruits/AdMob/RewardAdView.swift
@@ -0,0 +1,8 @@
+//
+//  RewardAdView.swift
+//  jugglingfruits
+//
+//  Created by Murilo Correa Pasquim on 22/04/24.
+//
+
+import Foundation
